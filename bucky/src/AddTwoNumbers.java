33450397b08cb86aa0cfdff7e37500725fed4389
kapil <kapiolhisar785@gmail.com>
2021-11-03 18:40:39 +0530

    change variables names

--- bucky/src/AddTwoNumbers.java
+++ bucky/src/AddTwoNumbers.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 public class AddTwoNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
-        System.out.println("first number");
-        int a = scanner.nextInt();
-        System.out.println("second number");
-        int b =scanner.nextInt();
-        System.out.println("third number");
-        int c = scanner.nextInt();
-        int d = a+b+c;
-        System.out.println(  +d);
+        System.out.println("first number ");
+        int firstNumber = scanner.nextInt();
+        System.out.println("second number ");
+        int secondNumber =scanner.nextInt();
+        System.out.println("third number ");
+        int thirdNumber = scanner.nextInt();
+        int total = firstNumber + secondNumber + thirdNumber;
+        System.out.println(total);
 
     }
 }
