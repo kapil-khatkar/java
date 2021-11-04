496c862b7add745dcc01bd5a412877b9abe93856
kapil <kapiolhisar785@gmail.com>
2021-11-04 23:21:34 +0530

    learn Increment And Decrement Operators

---
+++ bucky/src/IncrementAndDecrementOperators.java
@@ -0,0 +1,20 @@
+public class IncrementAndDecrementOperators {
+    public static void main(String[] args) {
+       int Sumit = 75;
+       int Naresh = 60;
+       int Kapil = 33;
+       Sumit += 7;
+       Naresh -= 5;
+       Kapil += 10;
+       System.out.println(Sumit);
+       System.out.println("Sumit");
+
+       System.out.println(Naresh);
+       System.out.println("Naresh");
+
+       System.out.println("kapil");
+       System.out.println(Kapil++);
+       System.out.println("Kapil post increment");
+       System.out.println(Kapil);
+    }
+}
