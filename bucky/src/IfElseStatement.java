1ac9e19d19a861faf58c52f2a2fb01b6b0c58227
kapil <kapiolhisar785@gmail.com>
2021-11-05 01:07:57 +0530

    variable changed from bean to sc

--- bucky/src/IfElseStatement.java
+++ bucky/src/IfElseStatement.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 public class IfElseStatement {
     public static void main(String[] args) {
-        Scanner bean = new Scanner(System.in);
-        int test = bean.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int test = sc.nextInt();
         if (test >= 6) {
             System.out.println("apple is fruit");
         } else {
