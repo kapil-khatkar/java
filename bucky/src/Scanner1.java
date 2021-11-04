d37020e66098cd99bab02fca30b4103392dc7a3a
kapil <kapiolhisar785@gmail.com>
2021-11-04 13:54:26 +0530

    learning Scanner1

---
+++ bucky/src/Scanner1.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Scanner1 {
+    public static void main(String[] args) {
+        Scanner kapil = new Scanner (System.in);
+        System.out.println(kapil.nextLine());
+
+    }
+}
