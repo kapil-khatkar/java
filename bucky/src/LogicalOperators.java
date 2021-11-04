fbd5bb16b1d7e2e12f6e7bd9312052371b567bbd
kapil <kapiolhisar785@gmail.com>
2021-11-04 15:10:40 +0530

    learn logical Operators

--- bucky/src/LogicalOperators.java
+++ bucky/src/LogicalOperators.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class LogicalOperators{
     public static void main(String[] args) {
         Scanner kapil = new Scanner(System.in);
-        int boys, girls, people, difference, division, module ;
+        int boys, girls, people, difference, division, module;
         boys  = 15;
-        girls =  7 ;
+        girls =  7;
         people = boys + girls;
         difference = boys - girls;
         division = boys / girls;
