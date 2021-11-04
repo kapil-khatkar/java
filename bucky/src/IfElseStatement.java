5ff9f2278e92dcea8d257789880632f333653df5
kapil <kapiolhisar785@gmail.com>
2021-11-05 00:27:50 +0530

    all names changes to small letters done

---
+++ bucky/src/IfElseStatement.java
@@ -0,0 +1,12 @@
+public class IfElseStatement {
+    public static void main(String[] args) {
+        int test = 5;
+        if (test < 5)
+        {
+            System.out.println("apple is fruit");
+        }else
+            {
+                System.out.println("apple is good for health");
+            }
+        }
+    }
